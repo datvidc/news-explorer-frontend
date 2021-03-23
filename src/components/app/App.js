22a26f052a8a2a085d4c887c2180a0b17e564b07
David Christoffersen <d@vidc.dk>
2021-03-23 16:03:27 -0400

    still fighting this stoopid eslint thing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,35 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: {},
+      // name: "Lacking Gravitas",
+      // about: "SPaceSHip", avatar: defaultAvatarPicture, _id" some id.
+    };
 
-    </div>
-  );
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem('jwt');
+
+    if (token) {
+      this.checkToken(token);
+      // now for generating cards
+    }
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
 }
-
 export default App;
+
+
