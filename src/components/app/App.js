f3320f994389f12472869b6bf17a8f2032bb498e
David Christoffersen <d@vidc.dk>
2021-03-27 14:51:09 -0400

    asdasd

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -38,6 +38,7 @@ const App = () => {
               <Main
                 device={UserWindow}
                 knownUser={Loggedin}
+                mainPage
               />
             </Route>
 
