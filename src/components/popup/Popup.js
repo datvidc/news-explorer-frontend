f3e6cb3d9525c137d11015476cd9012e29a396a3
David Christoffersen <d@vidc.dk>
2021-04-08 19:52:33 -0400

    Modal works

--- src/components/popup/Popup.js
+++ src/components/popup/Popup.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './Popup.css';
 
 function Popup(props) {
-  const { component, children } = props;
+  const { component, children, close } = props;
+
+  useEffect(() => {
+    const closeModal = (event) => {
+      if (event.keyCode === 27) {
+        close();
+      }
+    };
+    window.addEventListener('keydown', closeModal);
+    return () => window.removeEventListener('keydown', closeModal);
+  }, []);
+
   return (
     <section className="popup">
       {component}
