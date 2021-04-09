bd4ce92eb86e15f2248aed9e5653929f9d7b1e0a
David Christoffersen <d@vidc.dk>
2021-04-09 14:56:28 -0400

    BEM changes

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,20 +10,20 @@ function Footer() {
       <p className="footer__copytext"> © 2020 No/Mad, Powered by News API</p>
       <nav className="footer__nav">
         <ul className="footer__links">
-          <li className="footer_listItem footer__navilink">
+          <li className="footer__navilink">
             <a href="/"> Home </a>
           </li>
-          <li className="footer_listItem footer__navilink">
+          <li className="footer__navilink">
             <a href="https://practicum.yandex.com/web/"> Practicum by Yandex </a>
           </li>
         </ul>
         <ul className="footer__linkspro">
-          <li className="footer_listItem footer__pro">
+          <li className="footer__pro">
             <a href="https://github.com/datvidc">
               <img className="footer__img" src={github} alt="github" />
             </a>
           </li>
-          <li className="footer_listItem footer__pro">
+          <li className="footer__pro">
             <a href="https://www.linkedin.com/in/dacpo/">
               <img className="footer__img" src={Linkedin} alt="LinkedIn" />
             </a>
