307f2df997fe3827f80f4a444e82f3719e0ceaef
David Christoffersen <d@vidc.dk>
2021-04-08 22:04:29 -0400

    Loading

--- src/components/preloader/preloader.js
+++ src/components/preloader/preloader.js
@@ -1,6 +1,5 @@
 import React from 'react';
-
 import './preloader.css';
 
-const Loading = () => <i className="circle-preloader" />; // trying out some saucey new ES6 style
+const Loading = () => <i className="circle-preloader" />; // new saucey ES6 syntax...
 export default Loading;
