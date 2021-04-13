c51f0b6b57eac10aeb36b54fe6226d1a8614655e
David Christoffersen <d@vidc.dk>
2021-04-13 11:51:39 -0400

    Fleshing out our API

---
+++ src/utils/MainApi.js
@@ -0,0 +1,70 @@
+import {
+  API_MAIN,
+  API_KEY,
+  API_YANDEX,
+  API_NEWS,
+} from './secret/secret.json';
+
+class Api {
+  constructor() {
+    this._MainUrl = API_MAIN;
+    this._apiKey = API_KEY;
+    this._yandexApi = API_YANDEX;
+    this._newsApi = API_NEWS;
+  }
+
+  signIn(email, password) {
+    const userUrl = this._MainUrl.concat('/signin');
+    return fetch(userUrl, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.message) {
+          localStorage.setItem('jwt', data.token);
+        }
+        return data;
+      });
+  }
+
+  getCurrentUser(token) {
+    const userUrl = this._MainUrl.concat('/users/me');
+    return fetch(userUrl, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(new Error(`${res.status} : ${res.message}`));
+      }).catch((res) => {
+        console.log(res);
+      });
+  }
+
+  signup(email, password, name) {
+    const userUrl = this._MainUrl.concat('/signup');
+    return fetch(userUrl, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password, name }),
+    });
+  }
+}
+
+const api = new Api('http://api.nomad.students.nomoreparties.site');
+
+export default api;
